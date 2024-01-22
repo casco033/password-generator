f5ad7c7b12a0b68683035f4a354caaa4255b7718
Benjamin <casco033@gmail.com>
2024-01-22 01:04:39 -0700

    finished project and readme

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var choiceArr = []; 
 
+// Arrays containing the characters for the possible password
 var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numberArr = ["1","2","3","4","5","6","7","8","9"];
@@ -22,12 +23,16 @@ function writePassword() {
 
 }
 
-
+// Function to generate password based on prompts
 function generatePassword() {
 var result = [];
 var concatcharacters =[];
 var definitecharacters =[]; 
+
+// Get user prompts to determine password requirements 
 var correctPrompts = getPrompts();
+
+// Concatenate character arrays based on user prompts and ensure at least one of each type is included
 if (correctPrompts.lowercase) {
   concatcharacters =concatcharacters.concat(lowercaseArr)
   definitecharacters.push(getRandom(lowercaseArr))
@@ -48,6 +53,7 @@ if (correctPrompts.special) {
   definitecharacters.push(getRandom(specialCharArr))
 }
 
+// Generate the rest of the password characters based on total length
   for (var i = 0; i < correctPrompts.characterLength; i ++ ) {
     var concatcharacter = getRandom(concatcharacters); 
     result.push(concatcharacter)
@@ -55,20 +61,23 @@ if (correctPrompts.special) {
   for(var i = 0; i < definitecharacters.length; i ++ ){
     result[i] = definitecharacters[i] 
   }
+
+// Convert the password array to a string and return
   return result.join("")
 }
 
+// Get a random element from an array
 function getRandom(arr) {
-  
-    var randomIndex = Math.floor(Math.random() * arr.length)
-    var randomElement = arr[randomIndex]
-    return randomElement; 
+  var randomIndex = Math.floor(Math.random() * arr.length)
+  var randomElement = arr[randomIndex]
+  return randomElement; 
   
 }
 
 
 function getPrompts() {
 
+// Get password lenght   
    var characterLength = parseInt(prompt("How many characters would you like your password to contain? (8 - 128 characters")) ;
 
   if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
@@ -76,6 +85,7 @@ function getPrompts() {
     return false; 
   }
 
+// Get user characters preferences 
  var uppercase = confirm("Click OK if you would like to include uppercase characters.") 
 
 var lowercase = confirm ("Click OK if you would like to include lowercase characters.") 
@@ -83,7 +93,8 @@ var lowercase = confirm ("Click OK if you would like to include lowercase charac
 var number = confirm ("Click OK if you would like to include numeric characters.") 
   
 var special = confirm ("Click OK if you would like to include special characters.") 
-  
+
+// Store user preferences on an object and return 
 var passwordoptions = {
   characterLength : characterLength, 
   uppercase : uppercase, 
@@ -95,48 +106,3 @@ return passwordoptions;
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
